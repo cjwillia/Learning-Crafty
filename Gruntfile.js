27bf7850be66b729b26eab7cbbf8a6a8e491f993
Daniel Garcia <danny@noisenewyork.com>
2014-02-19 11:33:07 -0500

    Update gruntfile for local watching.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,10 +14,20 @@ module.exports = function(grunt) {
           Crafty: true
         }
       }
+    },
+    watch: {
+      src: {
+        files: ['src/**/*.js'],
+        tasks: ['jshint'],
+        options: {
+          spawn: false
+        }
+      }
     }
   });
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   
   grunt.registerTask('default', ['jshint']);
 
